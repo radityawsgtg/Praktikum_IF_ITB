e9d902c98d80ba7ee0f8f7fe48c33261e4afce63
Radityawsgtg <radityaws79@gmail.com>
2026-03-12 23:50:51 +0700

    feat: soal sti

---
+++ Leetcode/Laporanpisang.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int Cekkat (int a){
+    switch (a)
+    {
+    case 0:
+        printf("Mentah");
+        break;
+    case 1:
+        printf("Mentah");
+        break;
+    case 2:
+        printf("Mentah");
+        break;
+    case 3:
+        printf("Matang");
+        break;
+    case 4:
+        printf("Matang");
+        break;
+    case 5:
+        printf("Matang");
+        break;
+    case
+    
+}
